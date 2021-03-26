09ea05c127801495be1c5a259d23c14a1c940229
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-03-26 16:09:31 +0600

    add new file componaents Keypag

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import keypad from "./componants/Keypad"
+
+
+function App() {
+  return (
+    <div className="App">
+     <keypad/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componants/Keypad/Keypad.js
@@ -0,0 +1,8 @@
+import classes from "./keypad.module.css";
+const keypad = () => {
+    return <div className={classes.keypad}>
+      
+       
+    </div>;
+}
+export default keypad;
