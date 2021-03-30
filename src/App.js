1603349d3488908feb1aa91ee80c2ab4cfbce89e
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-03-30 14:01:58 +0600

    add in the file new function and other styli

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-
+import { useState } from "react";
 import Countdown from "./componants/Countdown/Countdown";
 
+const App = () => {
+  const [countdown, setCountdown] = useState(20);
+  const [a, setA] = useState(Math.round(Math.random() * 50));
+  const [b, setB] = useState(Math.round(Math.random() * 50));
+  const [c, setC] = useState("?");
 
-function App() {
+  function reset() {
+    setA(Math.round(Math.random() * 50));
+    setB(Math.round(Math.random() * 50));
+    setC("?");
+    setCountdown(20);
+  }
+
+  function checkAnswer() {
+    if (a + b == c) {
+      reset();
+    }
+    else {
+      alert("No");
+    }
+  }
+
   return (
     <div className="App">
-      <Countdown />
+      {a} + {b} = {c}
+      
+      <Countdown countdown={countdown}
+        setCountdown={setCountdown} />
+      <input type="text"
+        onChange={({ target }) => setC(target.value)} />
+      <button onClick={checkAnswer}>Ok</button>
     </div>
   );
 }
+
 export default App;
